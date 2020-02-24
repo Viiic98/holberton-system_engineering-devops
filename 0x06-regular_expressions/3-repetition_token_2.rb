584a87f81d28ecf772d7a3e24362687972b800d5
viiic98 <xathovic@gmail.com>
2020-02-24 18:00:17 +0000

    Fix the implementation

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # Print a word with a specific range of a letter
-for i in ARGV[0].scan(/hb[t]{0,}n/)
-    puts ARGV[0]
+for i in ARGV[0].scan(/hbt{1,}n/)
+    print ARGV[0]
 end
+print "\n"
