d542e0b027c7c5748e763e8380408de8ce11af28
viiic98 <xathovic@gmail.com>
2020-02-25 14:56:55 +0000

    Fix regex

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # Print a word with a specific range of a letter
 if ARGV[0]
-    for i in ARGV[0].scan(/^\d{10,10}/)
+    for i in ARGV[0].scan(/^\d{10,10}$/)
         print ARGV[0]
     end
 end
