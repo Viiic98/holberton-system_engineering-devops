916d6ee45e3b9f71fcfeeb175bf5d585b0ff31ef
Viiic98 <xathovic@gmail.com>
2020-05-20 18:09:14 -0500

    Fix some syntax errors

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
-file_line { 'WordPress apache2 500'
+# WordPress Error
+file_line { 'WordPress apache2 500':
   ensure => present,
   path   => '/var/www/html/wp-settings.php',
   line   => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );',
-  match  => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );',
+  match  => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );'
 }
