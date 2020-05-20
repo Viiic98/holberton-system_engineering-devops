ced3abea4bf8385a4f0939eb1c870e5fc53de254
Viiic98 <xathovic@gmail.com>
2020-05-20 17:53:47 -0500

    Add task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+file_line { 'WordPress apache2 500'
+  ensure => present,
+  path   => '/var/www/html/wp-settings.php',
+  line   => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );',
+  match  => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );',
+}
